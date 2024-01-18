2b6668d886dc8f5fa693c6f6ba89918457196954
LAPTOP-I0S7QNUN\Gleb <muravin9118@gmail.com>
2024-01-19 01:58:24 +0300

    video-finish

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-document.getElementById('play-button').addEventListener('click', function() {
-    var video = document.getElementById('custom-video');
-    
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
+var playButton = document.getElementById('play-button');
+var videoOverlay = document.getElementById('video-overlay');
+var closeButton = document.getElementById('close-button');
+var blackSquare = document.getElementById('black-square');
+var customVideo = document.getElementById('custom-video');
+
+playButton.addEventListener('click', function() {
+    blackSquare.style.display = 'none'; // Скрыть черный квадрат с кнопкой
+    videoOverlay.style.zIndex = 1000; // Поднять видео-оверлей на верхний уровень
+    videoOverlay.style.display = 'flex'; // Показать видео-оверлей
+    customVideo.play(); // Воспроизвести видео
+});
+
+closeButton.addEventListener('click', function() {
+    closeVideo();
+});
+
+document.addEventListener('click', function(event) {
+    if (event.target === videoOverlay) {
+        closeVideo();
     }
+});
+
+function closeVideo() {
+    blackSquare.style.display = 'flex'; // Показать черный квадрат с кнопкой
+    videoOverlay.style.zIndex = -1; // Опустить видео-оверлей на нижний уровень
+    videoOverlay.style.display = 'none'; // Скрыть видео-оверлей
+    customVideo.pause(); // Поставить видео на паузу
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeVideo();
+    }
+});
+
+customVideo.addEventListener('ended', function() {
+    blackSquare.style.display = 'flex'; // Показать черный квадрат с кнопкой после завершения видео
+    videoOverlay.style.zIndex = -1; // Опустить видео-оверлей на нижний уровень
+    videoOverlay.style.display = 'none'; // Скрыть видео-оверлей
 });
